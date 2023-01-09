1acb3048d2087d4c7a585edeead3898353fe6ff4
jaewoopk <jaewoopk000@naver.com>
2023-01-09 18:34:00 +0900

    Day 4 Solve slidingWindow Algorithm but It's not clearly understand

---
+++ Chapter3_prefixSum/slidingWindow.java
@@ -0,0 +1,115 @@
+package Chapter3_prefixSum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class slidingWindow {
+    static int s, p;
+    static int[] checkArr;
+    static int[] myArr;
+    static int checkSecret;
+    static int count;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        char[] arr = new char[s];
+        checkArr = new int[4];
+        myArr = new int[4];
+        count = 0;
+        checkSecret = 0;
+
+        s = Integer.parseInt(st.nextToken());
+        p = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        arr = st.nextToken().toCharArray();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            checkArr[i] = Integer.parseInt(st.nextToken());
+            if (checkArr[i] == 0) {
+                checkSecret++;
+            }
+        }
+
+        for (int i = 0; i < p; i++) {
+            Add(arr[i]);
+        }
+
+        if (checkSecret == 4) {
+            count++;
+        }
+    
+        for (int i = p; i < s; i++) {
+            int j = i - p;
+            Add(arr[i]);
+            Remove(arr[j]);
+            if (checkSecret == 4) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        
+    }
+
+    private static void Add(char c) {
+        switch (c) {
+        case 'A' :
+            myArr[0]++;
+            if (myArr[0] == checkArr[0]) {
+                checkSecret++;
+            }
+            break;
+        case 'C' :
+            myArr[1]++;
+            if (myArr[1] == checkArr[1]) {
+                checkSecret++;
+            }
+            break;
+        case 'G' :
+            myArr[2]++;
+            if (myArr[2] == checkArr[2]) {
+                checkSecret++;
+            }
+            break;
+        case 'T' :
+            myArr[3]++;
+            if (myArr[3] == checkArr[3]) {
+                checkSecret++;
+            }
+            break;
+        }
+    }
+
+    private static void Remove(char c) {
+        switch (c) {
+            case 'A' :
+                if (myArr[0] == checkArr[0]) {
+                    checkSecret--;
+                }
+                myArr[0]--;
+                break;
+            case 'C' :
+                if (myArr[1] == checkArr[1]) {
+                    checkSecret--;
+                }
+                myArr[1]--;
+                break;
+            case 'G' :
+                if (myArr[2] == checkArr[2]) {
+                    checkSecret--;
+                }
+                myArr[2]--;
+                break;
+            case 'T' :
+                if (myArr[3] == checkArr[3]) {
+                    checkSecret--;
+                }
+                myArr[3]--;
+                break;
+            }
+    }
+}
