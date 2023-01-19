658eb05d04e4c89c67c80d17b00e055f86bacbde
Ryan <jaewoopk000@naver.com>
2023-01-19 15:57:53 +0900

    Day 12 Solve DFS and BFS algorithm

--- Chapter5_Search/BFS.java
+++ Chapter5_Search/BFS.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -16,32 +17,29 @@ public class BFS {
     static void bfs(int idx) {
         visited[idx] = true;
         queue.add(idx);
-
-        int i = 0;
-        while (i < arr[idx].size() - 1 && !queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int p = queue.poll();
-            System.out.print(" " + p);
-            if (!visited[arr[p].get(i)]) {
-                visited[arr[p].get(i)] = true;
-                queue.add(arr[p].get(i));
-                bfs(arr[p].get(i));
+            int i = 0;
+            System.out.print(p + " ");
+            while (i < arr[p].size()) {
+                if (visited[arr[p].get(i)] == false) {
+                    queue.add(arr[p].get(i));
+                    visited[arr[p].get(i)] = true;
+                }
+                i++;
             }
-            i++;
         }
-        visited[idx] = false;
     }
 
     static void dfs(int idx) {
         visited[idx] = true;
-        System.out.print(" " + idx);
         for (int i = 0; i < arr[idx].size(); i++) {
             if (visited[arr[idx].get(i)] == true) {
                 continue ;
             }
+            System.out.print(arr[idx].get(i) + " ");
             dfs(arr[idx].get(i));
         }
-
-        visited[idx] = false;
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -52,15 +50,15 @@ public class BFS {
         m = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        arr = new ArrayList[m];
+        arr = new ArrayList[n + 1];
         queue = new LinkedList<Integer>();
-        visited = new boolean[n];
+        visited = new boolean[n + 1];
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 1; i < n + 1; i++) {
             arr[i] = new ArrayList<Integer>();
             visited[i] = false;
         }
-        for (int i = 0; i < m; i++) {
+        for (int i = 1; i < m + 1; i++) {
             int v, e;
             st = new StringTokenizer(br.readLine());
             v = Integer.parseInt(st.nextToken());
@@ -68,8 +66,15 @@ public class BFS {
             arr[v].add(e);
             arr[e].add(v);
         }
+        for (int i = 1; i < n + 1; i++) {
+            Collections.sort(arr[i]);
+        }
+        System.out.print(k + " ");
         dfs(k);
         System.out.println();
-        //bfs(k);
+        for (int i = 1; i < n + 1; i++) {
+            visited[i] = false;
+        }
+        bfs(k);
     }
 }
