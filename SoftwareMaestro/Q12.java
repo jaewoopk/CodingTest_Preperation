c591860b9c7bfdde9078f0b00507dec459b0ffda
jaewoopk <jaewoopk000@naver.com>
2023-02-17 18:17:08 +0900

    Day 27 Solve about Q12 revise with dp

--- SoftwareMaestro/Q12.java
+++ SoftwareMaestro/Q12.java
@@ -5,7 +5,8 @@ import java.io.*;
 
 public class Q12 {
     static int n, k;
-    static Integer[] arr;
+    static int[] arr;
+    static int[] dp;
     static int count;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,34 +16,18 @@ public class Q12 {
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        arr = new Integer[n];
-        for (int i = 0; i < n; i++) {
+        arr = new int[n + 1];
+        dp = new int[k + 1];
+        dp[0] = 1;
+        for (int i = 1; i < n + 1; i++) {
             st = new StringTokenizer(br.readLine());
             arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                // TODO Auto-generated method stub
-                return o2 - o1;
+            for (int j = arr[i]; j <= k; j++) {
+                dp[j] += dp[j - arr[i]];
             }
-        });
-
-        myGreedy(0, 0);
-        System.out.println(count);
+        }
+        System.out.println(dp[k]);
 
     }
- 
-    static void myGreedy(int sum, int idx) {
-        if (sum == k) {
-            count++;
-            return ;
-        }
-        else if (sum > k) {
-            return ;
-        }
-        for (int i = idx; i < n; i++) {
-            myGreedy(sum + arr[i], idx + 1);
-        }
-    }
+
 }
