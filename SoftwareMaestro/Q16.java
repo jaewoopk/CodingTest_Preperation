a1bf41fbed4be9b2b55bf85fa7b44174090911eb
jaewoopk <jaewoopk000@naver.com>
2023-02-20 15:23:16 +0900

    Day 28 Solve Q16 about BackTracking3

---
+++ SoftwareMaestro/Q16.java
@@ -0,0 +1,35 @@
+package SoftwareMaestro;
+
+import java.util.*;
+import java.io.*;
+
+public class Q16 {
+    private static int n, m;
+    private static int[] arr;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[10];
+
+        backTracking(1);
+    }
+
+    static void backTracking(int idx) {
+        if (idx == m + 1) {
+            for (int i = 1; i < m + 1; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return ;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            arr[idx] = i;
+            backTracking(idx + 1);
+        }
+    }
+}
