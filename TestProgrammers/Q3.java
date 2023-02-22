1ac5ad62b741720514790c64d3d2b7de1ae9774b
jaewoopk <jaewoopk000@naver.com>
2023-02-22 20:16:45 +0900

    Day 29 Solve Programmers Codingtest

---
+++ TestProgrammers/Q3.java
@@ -0,0 +1,41 @@
+package TestProgrammers;
+
+import java.util.PriorityQueue;
+
+public class Q3 {
+    class Solution {
+        public String solution(String s) {
+
+            Deque q = new PriorityQueue<>();
+            String answer = "";
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+            
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == ' ') {
+                    continue ;
+                }
+                if (s.charAt(i) == '-') {
+                    i++;
+                    if (-(int)(s.charAt(i)) - '0' > max) {
+                        max = -(int)(s.charAt(i) - '0');
+                    }
+                    if (-(int)(s.charAt(i)) - '0' < min) {
+                        min = -(int)(s.charAt(i) - '0');
+                    }    
+                }
+                else {
+                    if ((int)(s.charAt(i)) - '0' > max) {
+                        max = (int)(s.charAt(i) - '0');
+                    }
+                    if ((int)(s.charAt(i)) - '0' < min) {
+                        min = (int)(s.charAt(i) - '0');
+                    }   
+                }
+            }
+            answer = Integer.toString(min) + " " + Integer.toString(max);
+            System.out.println(min + " " + max);
+            return answer;
+        }
+    }
+}
