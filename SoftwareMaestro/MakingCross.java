03a9a6d9ba038a028b6a5e2e6777eba8abf8bbba
jaewoopk <jaewoopk000@naver.com>
2023-02-26 16:37:43 +0900

    Day 32 Review Coding Test

---
+++ SoftwareMaestro/MakingCross.java
@@ -0,0 +1,14 @@
+package SoftwareMaestro;
+
+public class MakingCross {
+
+    static void recursive(int len) {
+
+    }
+    public static void main(String[] args) {
+        int n, m;
+        int[][] board = new int[101][101];
+
+        recursive(0);
+    }
+}
