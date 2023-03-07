eb89a33c3857fa31ee22c4484526e7ef32b70dd2
jaewoopk <jaewoopk000@naver.com>
2023-03-07 17:52:39 +0900

    Day 34 Not yet for solve Q2

---
+++ Programmers_Question/Question2.java
@@ -0,0 +1,15 @@
+package Programmers_Question;
+
+public class Question2 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Solution {
+    public int solution(String[] board) {
+        int answer = -1;
+        return answer;
+    }
+}
