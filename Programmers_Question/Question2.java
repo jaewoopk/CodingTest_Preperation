e538cb24eb50bc49335defe405444a5389082a1b
jaewoopk <jaewoopk000@naver.com>
2023-03-08 17:31:54 +0900

    Day 35 Trying to solve Q2

--- Programmers_Question/Question2.java
+++ Programmers_Question/Question2.java
@@ -1,5 +1,7 @@
 package Programmers_Question;
 
+import java.util.ArrayList;
+
 public class Question2 {
     public static void main(String[] args) {
 
@@ -9,7 +11,31 @@ public class Question2 {
 
 class Solution {
     public int solution(String[] board) {
-        int answer = -1;
+        int answer = 1;
+        ArrayList<String> idxArr = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i].charAt(j) == 'O') {
+                    idxArr.add("O");
+                }
+                else if (board[i].charAt(j) == 'X') {
+                    idxArr.add("X");
+                }
+            }
+        }
+        int cnt = 0;
+        for (var e : idxArr) {
+            if (e.equals("O")) {
+                cnt++;
+            }
+            else {
+                cnt--;
+            }
+        }
+        if (cnt != 0) {
+            answer = 0;
+        }
         return answer;
     }
+    
 }
