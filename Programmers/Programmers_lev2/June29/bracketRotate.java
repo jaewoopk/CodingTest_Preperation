69b09b26e019a6ae2d4945c6c8b1535ea9858a22
jaewoopk <jaewoopk000@naver.com>
2023-06-30 15:13:47 +0900

    Day 49 Revise bracketRotate with Deque and Collections and Map

--- Programmers/Programmers_lev2/June29/bracketRotate.java
+++ Programmers/Programmers_lev2/June29/bracketRotate.java
@@ -30,10 +30,7 @@ class Solution629_1 {
     public static int solution(String s) {
         int answer = 0;
         Deque<Character> dq = new ArrayDeque<Character>();
-        
-        for (char e : s.toCharArray()) {
-            dq.add(e);
-        }
+        Collections.addAll(dq, s.chars().mapToObj(c -> (char) c).toArray(Character[]::new));
 
         for (int i = 0; i < s.length(); i++) {
             if (isCorrect(dq)) {
@@ -46,3 +43,47 @@ class Solution629_1 {
         return answer;
     }
 }
+
+// import java.util.*;
+
+// public class BracketRotate {
+//     public static void main(String[] args) {
+//         String str = "[](){}";
+//         System.out.println(Solution629_1.solution(str));
+//     }
+// }
+
+// class Solution629_1 {
+//     private static final Map<Character, Character> brackets = Map.of(']', '[', ')', '(', '}', '{');
+
+//     public static boolean isCorrect(Deque<Character> str) {
+//         Stack<Character> stack = new Stack<Character>();
+
+//         for (char ch : str) {
+//             if (brackets.containsValue(ch)) {
+//                 stack.push(ch);
+//             } else if (stack.isEmpty() || stack.pop() != brackets.get(ch)) {
+//                 return false;
+//             }
+//         }
+
+//         return stack.isEmpty();
+//     }
+
+//     public static int solution(String s) {
+//         int answer = 0;
+//         Deque<Character> dq = new ArrayDeque<Character>(s.length());
+//         for (char ch : s.toCharArray()) {
+//             dq.add(ch);
+//         }
+
+//         for (int i = 0; i < s.length(); i++) {
+//             if (isCorrect(dq)) {
+//                 answer++;
+//             }
+//             dq.addLast(dq.pollFirst());
+//         }
+
+//         return answer;
+//     }
+// }
