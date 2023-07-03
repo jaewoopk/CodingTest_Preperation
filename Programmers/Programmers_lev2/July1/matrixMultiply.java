90df94e18e2342657e2ec904406f8f4bbe086121
jaewoopk <jaewoopk000@naver.com>
2023-07-03 16:44:36 +0900

    Day 51 Solve matrix multiply with 3 for (Math)

--- Programmers/Programmers_lev2/July1/matrixMultiply.java
+++ Programmers/Programmers_lev2/July1/matrixMultiply.java
@@ -2,7 +2,7 @@ package Programmers_lev2.July1;
 
 public class matrixMultiply {
     public static void main(String[] args) {
-        Solution701_2.solution(new int[][] {{1,4}, {3,2}, {4,1}}, new int[][] {{3,3}, {3,3}, {3,3}});
+        Solution701_2.solution(new int[][] {{1,4}, {3,2}, {4,1}}, new int[][] {{3,3, 1}, {3,3, 1}});
     }
 }
 
@@ -11,7 +11,7 @@ class Solution701_2 {
         int[][] answer = new int[arr1.length][arr2[0].length];
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2[0].length; j++) {
-                for (int k = 0; k < arr2[0].length; k++) {
+                for (int k = 0; k < arr2.length; k++) {
                     answer[i][j] += (arr1[i][k] * arr2[k][j]);
                 }
             }
