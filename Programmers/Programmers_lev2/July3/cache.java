ad32ae72e58262d52862d1d2fe825ed9a77bd93d
jaewoopk <jaewoopk000@naver.com>
2023-07-04 12:55:02 +0900

    Day 52 Solve cacheLRU with LinkedList

--- Programmers/Programmers_lev2/July3/cache.java
+++ Programmers/Programmers_lev2/July3/cache.java
@@ -11,7 +11,9 @@ public class cache {
 class Solution703_1 {
     public static int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
         LinkedList<String> list = new LinkedList<>();
         for (var city : cities) {
             city = city.toLowerCase();
@@ -31,3 +33,83 @@ class Solution703_1 {
         return answer;
     }
 }
+
+// import java.util.LinkedList;
+
+// class Solution {
+//     public int solution(int cacheSize, String[] cities) {
+//         Database database = new Database(cacheSize);
+
+//         int time = database.search(cities);
+
+//         return time;
+//     }
+// }
+
+// class Database {
+//      private final Cache cache;
+
+//     public Database(int cacheSize) {
+//         this.cache = new Cache(cacheSize);
+//     }
+
+//     public int search(String[] cities) {
+//         int hit = 0;
+//         int miss = 0;
+
+//         for (String city : cities) {
+//             if (cache.match(city.toLowerCase())) {
+//                 hit += 1;
+
+//                 continue;
+//             }
+
+//             miss += 1;
+//         }
+
+//         return getProcessTime(hit, miss);
+//     }
+
+//     private int getProcessTime(int hit, int miss) {
+//         return hit * 1 + miss * 5;
+//     }
+// }
+
+// class Cache {
+//     private int size;
+//     private LinkedList<String> storage = new LinkedList<>();
+
+//     public Cache(int cacheSize) {
+//         this.size = cacheSize;
+//     }
+
+//     public boolean match(String city) {
+//         if (size == 0) {
+//             return false;
+//         }
+
+//         if (storage.contains(city)) {
+//             if (storage.size() == size) {
+//                 storage.remove(storage.lastIndexOf(city));
+//                 storage.addFirst(city);
+
+//                 return true;
+//             }
+
+//             storage.addFirst(city);
+
+//             return true;
+//         }
+
+//         if (storage.size() == size) {
+//             storage.removeLast();
+//             storage.addFirst(city);
+
+//             return false;
+//         }
+
+//         storage.addFirst(city);
+
+//         return false;
+//     }
+// }
