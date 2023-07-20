db886ea3920d07464d4f726ef1983bdee6d1c212
jaewoopk <jaewoopk000@naver.com>
2023-07-20 19:21:55 +0900

    Day 66 Trying to solve Q2644_@

--- Baekjoon/Q2644/Q2644_degree.java
+++ Baekjoon/Q2644/Q2644_degree.java
@@ -6,19 +6,49 @@ import java.io.*;
 public class Q2644_degree {
     static int n, m;
     static int num1, num2;
-    static HashMap<Integer, ArrayList<Integer>> map;
+    static HashMap<Integer, LinkedList<Integer>> map;
+    static boolean[] visited;
 
+    public static int bfs(int start, int end) {
+        Queue<Integer> q = map.getOrDefault(start, new LinkedList<Integer>());
+        visited[start] = true;
+        int count = 0;
+
+        while (!q.isEmpty()) {
+            int e = q.poll();
+            if (!visited[e]) {
+                visited[e] = true;
+                LinkedList<Integer> list = map.getOrDefault(e, null);
+                count++;
+                for (int s : list) {
+                    if (visited[s]) {
+                        count--;
+                    }
+                }
+            }
+            if (e == end) return count;
+        }
+        return -1;
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+        map = new HashMap<>();
         n = sc.nextInt();
         num1 = sc.nextInt();
         num2 = sc.nextInt();
         m = sc.nextInt();
+        visited = new boolean[n + 1];
 
         for (int i = 0; i < m; i++) {
+            int key = sc.nextInt();
+            int value = sc.nextInt();
 
+            map.computeIfAbsent(key, k -> new LinkedList<>()).add(value);
+            map.computeIfAbsent(value, k -> new LinkedList<>()).add(key);
         }
+
+        System.out.println(bfs(num1, num2));
         sc.close();
     }
 }
