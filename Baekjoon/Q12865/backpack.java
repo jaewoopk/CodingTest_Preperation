39c68b658cbf34dab7b2b2bcd2fa238222a65b4e
jaewoopk <jaewoopk000@naver.com>
2023-07-26 15:43:02 +0900

    Day 72 Solve Q12865 using 2 dimension dp

--- Baekjoon/Q12865/backpack.java
+++ Baekjoon/Q12865/backpack.java
@@ -4,51 +4,39 @@ import java.io.*;
 import java.util.*;
 
 public class backpack {
-    static HashMap<Integer, Integer> map;
-    static ArrayList<Integer> list;
-    static int n, k, w, v, max = 0;
-    public static void backtracking(int numbers, int sum, int idx) {
-        if (numbers <= k && sum > max) {
-            max = sum;
-        }
-        for (int i = idx; i < list.size(); i++) {
-            if (numbers + i <= k) {
-                backtracking(numbers + list.get(i), sum + list.);
-            }
-        }
-    }
+    static int n, k;
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        map = new HashMap<>();
 
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            w = Integer.parseInt(st.nextToken());
-            v = Integer.parseInt(st.nextToken());
+        int[] weight = new int[n + 1];
+        int[] price = new int[n + 1];
+        int[][] dp = new int[n + 1][k + 1];
 
-            map.put(w, Math.max(map.getOrDefault(w,0),v));
+        for (int i = 1; i < n + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            weight[i] = Integer.parseInt(st.nextToken());
+            price[i] = Integer.parseInt(st.nextToken());
         }
-        list = new ArrayList<>(map.keySet());
-        Collections.sort(list);
+        // weight = 6  4 3 5
+        // price  = 13 8 6 12
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < k + 1; j++) {
+                if (weight[i] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + price[i]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                //System.out.print(dp[i][j] + " ");
+            }
+            //System.out.println();
+        }
+        System.out.println(dp[n][k]);
 
     }
 }
-
-// 1 2 3 4 5 6 7   ->> 10
-// 1 2 3 4
-// 1 2 4
-// 1 2 5
-// 1 2 6
-// 1 2 7
-// 1 3 4
-// 1 3 5
-// 1 3 6
-// 1 4 5
-// 1 5
-// 1 6
-// 1 7
-// 2 3 4
