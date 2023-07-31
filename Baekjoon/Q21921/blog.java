9557e945ac859952162744194dc463fe56f56c42
jaewoopk <jaewoopk000@naver.com>
2023-07-31 19:06:15 +0900

    Day 75 Revise Q21921 with 주석

--- Baekjoon/Q21921/blog.java
+++ Baekjoon/Q21921/blog.java
@@ -1,7 +1,21 @@
+/**
+ *  [ 효율성 ]
+ *  - 메모리: 52688KB
+ *  - 시간 : 676ms
+ */
+
 package Baekjoon.Q21921;
 
 import java.util.*;
 import java.io.*;
+
+/**
+ * 1. 누적합 배열을 선언합니다.
+ * 2. 누적합 배열을 사용해 key값을 도출합니다. (x 연속된 날들의 방문자 수)
+ * 3. HashMap<Integer, Integer>에 key 값이 없을 경우 1, 있을 경우 value에 +1을 해줍니다.
+ * 4. map.keySet()을 리스트에 담고 오름차순 정렬을 해줍니다.
+ * 5. 정렬된 상태에서 마지막 값이 0이면 "SAD"를, 아닐 경우 key값과 value값을 출력해줍니다.
+ */
 
 public class blog {
     public static void main(String[] args) throws IOException{
