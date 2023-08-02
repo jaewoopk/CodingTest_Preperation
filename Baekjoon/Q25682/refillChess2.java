0b2e36cd53ee3d615dd0752f0733985eb100f6b1
jaewoopk <jaewoopk000@naver.com>
2023-08-02 17:23:06 +0900

    Day 77 Trying to solve Q25682 but not yet2 and try it about 2 diversion array

--- Baekjoon/Q25682/refillChess2.java
+++ Baekjoon/Q25682/refillChess2.java
@@ -12,19 +12,21 @@ public class refillChess2 {
         int min = Integer.MAX_VALUE;
 
         for (int i = 0; i < n - k + 1; i++) {
-            int countIfBlackFirst = 0;
-            int countIfWhiteFirst = 0;
             for (int j = k; j < m + 1; j++) {
-                if (i % 2 == 0) {
-                    countIfBlackFirst += (sumBoard[i][j] - sumBoard[i][j - k]) - (k + 1) / 2;
-                    countIfWhiteFirst += (sumBoard[i][j] - sumBoard[i][j - k]) - k / 2;
-                } else {
-                    countIfBlackFirst += (sumBoard[i][j] - sumBoard[i][j - k]) - k / 2;
-                    countIfWhiteFirst += (sumBoard[i][j] - sumBoard[i][j - k]) - (k + 1) / 2;
+                int countIfBlackFirst = 0;
+                int countIfWhiteFirst = 0;
+                for (int h = 0; h < k; h++) {
+                    if (h % 2 == 0) {
+                        countIfBlackFirst += (sumBoard[h + i][j] - sumBoard[h + i][j - k]) - (k + 1) / 2;
+                        countIfWhiteFirst += (sumBoard[h + i][j] - sumBoard[h + i][j - k]) - k / 2;
+                    } else {
+                        countIfBlackFirst += (sumBoard[h + i][j] - sumBoard[h + i][j - k]) - k / 2;
+                        countIfWhiteFirst += (sumBoard[h + i][j] - sumBoard[h + i][j - k]) - (k + 1) / 2;
+                    }
                 }
+                System.out.println(countIfBlackFirst + " " + countIfWhiteFirst);
+                min = Math.min(min, Math.min(countIfBlackFirst, countIfWhiteFirst));
             }
-            System.out.println(countIfBlackFirst + " " + countIfWhiteFirst);
-            min = Math.min(min, Math.min(countIfBlackFirst, countIfWhiteFirst));
         }
         return min;
     }
@@ -36,20 +38,19 @@ public class refillChess2 {
         m = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        board = new int[n][m + 1];
-        sumBoard = new int[n][m + 1];
+        board = new int[n + 2][m + 2];
+        sumBoard = new int[n + 2][m + 2];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n + 1; i++) {
             int[] row = Arrays.stream(br.readLine().split("")).mapToInt(s -> s.equals("B") ? 1 : 0).toArray();
             System.arraycopy(row, 0, board[i], 1, m);
             for (int j = 1; j < m + 1; j++) {
-                sumBoard[i][j] = sumBoard[i][j - 1] + board[i][j];
+                sumBoard[i][j] = sumBoard[i][j - 1] + sumBoard[i - 1][j] - sumBoard[i - 1][j - 1] + board[i][j];
             }
         }
 
-        System.out.println(findCount());
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m + 1; j++) {
+        for (int i = 0; i < n + 2; i++) {
+            for (int j = 0; j < m + 2; j++) {
                 System.out.print(sumBoard[i][j] + " ");
             }
             System.out.println();
