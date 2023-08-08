e0172d29bea9ced01e722d6eb26f816f36f721ac
jaewoopk <jaewoopk000@naver.com>
2023-08-08 17:52:47 +0900

    Day 80 Solve Q1339 named findWord about Greedy

---
+++ Baekjoon/Q1339/findWord.java
@@ -0,0 +1,40 @@
+package Baekjoon.Q1339;
+
+import java.io.*;
+import java.util.*;
+
+public class findWord {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] alpha = new int[26];
+        for (int i = 0; i < N; i++) {
+            char[] word = br.readLine().toCharArray();
+
+            int pow = (int) Math.pow(10, word.length - 1);
+            for (char c : word) {
+                alpha[c - 'A'] += pow;
+                pow /= 10;
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            System.out.println(alpha[i]);
+            if (alpha[i] != 0) {
+                list.add(alpha[i]);
+            }
+        }
+
+        list.sort(Collections.reverseOrder());
+
+        System.out.println(list.toString());
+        int ans = 0;
+        int num = 9;
+        for (int val : list) {
+            ans += val * num--;
+        }
+        System.out.println(ans);
+    }
+}
