eae48fbbcefe25cdb469cc2bd6aaf9690f947d2b
jaewoopk <jaewoopk000@naver.com>
2023-08-18 11:52:12 +0900

    Day 87 Revise Programmers Lev3 BestSet with math

--- Programmers/Programmers_lev3/BestSet/bestSet.java
+++ Programmers/Programmers_lev3/BestSet/bestSet.java
@@ -31,3 +31,32 @@ class Solution {
         return answer;
     }
 }
+
+// 더 간결하게
+
+//package Programmers_lev3.BestSet;
+//
+//        import java.util.Arrays;
+//
+//public class bestSet {
+//    public static void main(String[] args) {
+//        System.out.println(Arrays.toString(Solution.solution(2, 9)));
+//        System.out.println(Arrays.toString(Solution.solution(3, 10)));
+//        System.out.println(Arrays.toString(Solution.solution(4, 82)));
+//    }
+//}
+//
+//class Solution {
+//    public static int[] solution(int n, int s) {
+//        if (s < n) return new int[] {-1};
+//
+//        int[] answer = new int[n];
+//        Arrays.fill(answer, s / n);
+//
+//        for (int i = n - 1; i >= n - s % n; i--) {
+//            answer[i]++;
+//        }
+//
+//        return answer;
+//    }
+//}
