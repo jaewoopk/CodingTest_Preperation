dbf3a9837c0a424c8bd2bb07dca579800b08c8df
jaewoopk <jaewoopk000@naver.com>
2023-08-20 11:20:52 +0900

    Day 88 network

---
+++ Programmers/Programmers_lev2/Network/network.java
@@ -0,0 +1,24 @@
+package Programmers_lev2.Network;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class network {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public static int solution(int n, int[][] computers) {
+        int answer = 0;
+
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < computers.length; i++) {
+            for (int j = 0; j < computers[i].length; j++) {
+
+            }
+        }
+        return answer;
+    }
+}
