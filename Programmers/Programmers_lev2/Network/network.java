934bd7f67fbd52449c5fde850ce9c58675e41e27
jaewoopk <jaewoopk000@naver.com>
2023-08-21 15:14:17 +0900

    Day 89 Solve and Revise Programmers Lev2 name about Network with dfs

--- Programmers/Programmers_lev2/Network/network.java
+++ Programmers/Programmers_lev2/Network/network.java
@@ -1,22 +1,46 @@
 package Programmers_lev2.Network;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class network {
     public static void main(String[] args) {
-
+        System.out.println(Solution.solution(3, new int[][] {{1,1,0},{1,1,0},{0,0,1}}));
+        System.out.println(Solution.solution(3, new int[][] {{1,1,0},{1,1,1},{0,1,1}}));
     }
 }
 
 class Solution {
+    static boolean[] visited;
+    static HashMap<Integer, List<Integer>> map;
+
+    public static void dfs(int idx) {
+        for (int i = 0; i < map.get(idx).size(); i++) {
+            if (!visited[map.get(idx).get(i)]) {
+                visited[map.get(idx).get(i)] = true;
+                dfs(map.get(idx).get(i));
+            }
+        }
+    }
     public static int solution(int n, int[][] computers) {
         int answer = 0;
 
-        List<List<Integer>> list = new ArrayList<>();
-        for (int i = 0; i < computers.length; i++) {
-            for (int j = 0; j < computers[i].length; j++) {
+        map = new HashMap<>();
+        visited = new boolean[computers[0].length];
 
+        for (int i = 0; i < computers.length; i++) {
+            List<Integer> list = new LinkedList<>();
+            for (int j = 0; j < computers[i].length; j++) {
+                if (i != j && computers[i][j] == 1) {
+                    list.add(j);
+                }
+            }
+            map.put(i, list);
+            //System.out.println(Arrays.toString(list.toArray()));
+        }
+        for (int i = 0; i < map.size(); i++) {
+            if (!visited[i]) {
+                dfs(i);
+                answer++;
             }
         }
         return answer;
