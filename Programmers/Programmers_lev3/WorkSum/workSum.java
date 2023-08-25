913a4bb64d266f4e13b2e8b78cef9b569d103d29
jaewoopk <jaewoopk000@naver.com>
2023-08-25 16:08:09 +0900

    Day 92 Solve Programmers Lev3 named workSum with PriorityQueue about Collections.reverseOrder() and Arrays.stream

--- Programmers/Programmers_lev3/WorkSum/workSum.java
+++ Programmers/Programmers_lev3/WorkSum/workSum.java
@@ -1,28 +1,30 @@
 package Programmers_lev3.WorkSum;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class workSum {
     public static void main(String[] args) {
         System.out.println(Solution.solution(4, new int[] {4,3,3}));
+        System.out.println(Solution.solution(1, new int[] {2,1,2}));
+        System.out.println(Solution.solution(3, new int[] {1,1}));
+        System.out.println(Solution.solution(12, new int[] {2,5,16}));
     }
 }
 
 class Solution {
     public static long solution(int n, int[] works) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        long answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int sum = Arrays.stream(works).sum();
 
         for (int e : works) {
             pq.add(e);
         }
-
-        while (n > 0) {
-
+        while (n-- > 0 && sum-- > 0) {
+            pq.add(pq.poll() - 1);
         }
 
-
-        return answer;
+        return pq.stream().mapToLong(i -> i * i).sum();
     }
 }
