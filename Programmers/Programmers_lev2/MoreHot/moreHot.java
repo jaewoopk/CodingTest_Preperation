114e76be406c7cd6501cf657b208b87d447d2bd5
jaewoopk <jaewoopk000@naver.com>
2023-08-26 19:49:26 +0900

    Day 93 Trying to solve Question named MoreHot with Heap or PriorityQueue but failed

---
+++ Programmers/Programmers_lev2/MoreHot/moreHot.java
@@ -0,0 +1,32 @@
+package Programmers_lev2.MoreHot;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+
+public class moreHot {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(new int[] {1,2,3,9,10,12},7));
+    }
+}
+
+class Solution {
+    public static int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Arrays
+                .stream(scoville)
+                .boxed()
+                .collect(Collectors.toList()));
+        int answer = 1;
+        int hot = pq.poll() + (pq.poll() * 2);
+
+        while (hot < K && !pq.isEmpty()) {
+            pq.add(hot);
+            hot = pq.poll() + (pq.poll() * 2);
+            answer++;
+        }
+        if (pq.isEmpty()) {
+            return -1;
+        }
+        return answer;
+    }
+}
