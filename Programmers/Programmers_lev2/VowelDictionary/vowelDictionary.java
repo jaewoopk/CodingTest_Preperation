d5862a88bf9f71302a013451679dca04098c5c0d
jaewoopk <jaewoopk000@naver.com>
2023-09-06 15:19:02 +0900

    Day 100 Solve Programmers Lev 2 named vowelDictionary with dfs algorithm name perfect search

--- Programmers/Programmers_lev2/VowelDictionary/vowelDictionary.java
+++ Programmers/Programmers_lev2/VowelDictionary/vowelDictionary.java
@@ -1,38 +1,60 @@
 package Programmers_lev2.VowelDictionary;
 
+import java.util.Arrays;
+
 public class vowelDictionary {
     public static void main(String[] args) {
-        System.out.println(Solution.solution("AAAAE"));
-        System.out.println(Solution.solution("EIO"));
+        System.out.println(Solution.solution("I"));
     }
 }
 
 class Solution {
-    static int idx = 1;
-    static String[] dic;
+    static int idx = 0;
+    static Character[][] dic;
     static Character[] str;
+
     public static void dfs(int len) {
         if (len >= 5) return;
         for (int i = 0; i < 5; i++) {
             switch (i) {
                 case 0 -> str[len] = 'A';
-                case 1 -> str[len] = 'A';
-                case 2 -> str[len] = 'A';
-                case 3 -> str[len] = 'A';
-                case 4 -> str[len] = 'A';
+                case 1 -> str[len] = 'E';
+                case 2 -> str[len] = 'I';
+                case 3 -> str[len] = 'O';
+                case 4 -> str[len] = 'U';
             }
-            System.out.println(str);
-            dic[idx++] = str;
-            dfs(new String(str), len + 1);
+            dic[idx++] = str.clone();
+            dfs(len + 1);
+            str[len] = ' ';
         }
 
     }
     public static int solution(String word) {
-        dic = new String[3906];
-        str = new Character[5];
-        dfs(new String(), 0);
+        while (word.length() < 5) {
+            word += " ";
+        }
 
-        int answer = 0;
-        return answer;
+        char[] answer = new char[5];
+        str = new Character[5];
+        dic = new Character[3906][5];
+
+        //init
+        for (int i = 0; i < 5; i++) {
+            if (word.charAt(i) >= 'A' && word.charAt(i) <= 'Z') {
+                answer[i] = word.charAt(i);
+            } else {
+                answer[i] = ' ';
+            }
+            str[i] = ' ';
+        }
+
+        dfs(0);
+
+        for (int i = 0; i < 3906; i++) {
+            if (Arrays.toString(dic[i]).equals(Arrays.toString(answer))) {
+                return i + 1;
+            }
+        }
+        return 0;
     }
 }
