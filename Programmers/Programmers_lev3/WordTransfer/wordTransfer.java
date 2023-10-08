023ff7e6b9040389f4605964d2d6c895b9e993a0
jaewoopk <jaewoopk000@naver.com>
2023-10-08 23:38:38 +0900

    Day 104 Solve Programmers Lev 3 which named WordTransfer with bfs, Not using IDE

--- Programmers/Programmers_lev3/WordTransfer/wordTransfer.java
+++ Programmers/Programmers_lev3/WordTransfer/wordTransfer.java
@@ -3,12 +3,15 @@ package Programmers_lev3.WordTransfer;
 import java.util.*;
 
 public class wordTransfer {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution("hit", "cog", new String[] {"hot", "dot", "dog", "lot", "log", "cog"}));
+    }
 }
 
 class Solution {
-    boolean[] visited;
+    static boolean[] visited;
 
-    public boolean isContain(String target, String[] words) {
+    static public boolean isContain(String target, String[] words) {
         for (int i = 0; i < words.length; i++) {
             if (words[i].equals(target)) {
                 return true;
@@ -17,23 +20,41 @@ class Solution {
         return false;
     }
 
-    public int bfs(String begin, String target, String[] words) {
+    static public boolean wordCheck(String tmp, String arg) {
+        int check = 0;
+        for (int i = 0; i < tmp.length(); i++) {
+            if (tmp.charAt(i) != arg.charAt(i)) {
+                check++;
+            }
+        }
+        if (check == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    static public int bfs(String begin, String target, String[] words) {
         Queue<Node> q = new LinkedList<>();
-        q.add(Node(0, begin));
+        q.add(new Node(0, begin));
 
         while (!q.isEmpty()) {
             Node tmp = q.poll();
 
             for (int i = 0; i < words.length; i++) {
-                if (!visited[i] && ) {
-
+                if (!visited[i] && wordCheck(tmp.word, words[i])) {
+                    visited[i] = true;
+                    q.add(new Node(tmp.depth + 1, words[i]));
+                }
+                if (visited[i] && words[i].equals(target)) {
+                    return tmp.depth + 1;
                 }
             }
         }
+        return 0;
     }
 
 
-    public int solution(String begin, String target, String[] words) {
+    static public int solution(String begin, String target, String[] words) {
         if (!isContain(target, words)) {
             return 0;
         }
