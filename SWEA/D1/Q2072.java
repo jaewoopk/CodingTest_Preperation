ce271951c8f923bf7d7f69043a295f91239fe5a6
jaewoopk <jaewoopk000@naver.com>
2023-10-09 15:31:20 +0900

    Day 105 Solve D1 Q2072 with implementation, No IDE

---
+++ SWEA/D1/Q2072.java
@@ -0,0 +1,26 @@
+package SWEA.D1;
+
+import java.util.Scanner;
+
+public class Q2072 {
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int[] arr = new int[10];
+            int sum = 0;
+            for (int i = 0; i < 10; i++) {
+                arr[i] = sc.nextInt();
+                if (arr[i] % 2 == 1) {
+                    sum += arr[i];
+                }
+            }
+            System.out.println("#" + test_case + " " + sum);
+        }
+    }
+}
