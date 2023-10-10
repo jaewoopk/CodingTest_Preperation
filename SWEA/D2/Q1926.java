08e6f28b3bb76ae5b8d86d0b7a38536dba4349fa
jaewoopk <jaewoopk000@naver.com>
2023-10-10 14:03:03 +0900

    Day 106 Solve Q1926 with easy implementation, No IDE

---
+++ SWEA/D2/Q1926.java
@@ -0,0 +1,33 @@
+package SWEA.D2;
+
+import java.util.*;
+
+public class Q1926 {
+    public static void main(String args[]) throws Exception	{
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++) {
+            boolean checker = false;
+            if (test_case % 10 == 3 || test_case % 10 == 6 || test_case % 10 == 9) {
+                checker = true;
+                sb.append("-");
+            }
+            if (test_case / 10 == 3 || test_case / 10 == 6 || test_case / 10 == 9) {
+                checker = true;
+                sb.append("-");
+            }
+            if (test_case / 100 == 3 || test_case / 100 == 6 || test_case / 100 == 9) {
+                checker = true;
+                sb.append("-");
+            }
+            if (!checker) {
+                sb.append(Integer.toString(test_case));
+            }
+            sb.append(" ");
+        }
+        System.out.println(sb);
+    }
+}
