28648c4e3f0fbdda01114c34c1f300409a7d5bd4
jaewoopk <jaewoopk000@naver.com>
2023-11-06 18:26:48 +0900

    Day 120 Solve Q17609 but it has time overflow

---
+++ Baekjoon/Q17609/Q17609.java
@@ -0,0 +1,50 @@
+package Baekjoon.Q17609;
+
+import java.util.*;
+import java.io.*;
+
+public class Q17609 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < T; i++) {
+            String str = br.readLine();
+            int len = str.length();
+            StringBuffer sb = new StringBuffer(str.substring(len / 2, len));
+            String cmpStr = sb.reverse().toString();
+
+            if (cmpStr.equals(str.substring(0, len / 2))) {
+                System.out.println(0);
+            } else {
+                boolean check = false;
+                for (int j = 0; j < str.length(); j++) {
+                    String newStr = str.substring(0, j).concat(str.substring(j + 1, str.length()));
+                    //System.out.println(newStr);
+                    sb = new StringBuffer(newStr.substring(len / 2, len - 1));
+                    cmpStr = sb.reverse().toString();
+
+                    if (cmpStr.equals(newStr.substring(0, len / 2))) {
+                        System.out.println(1);
+                        check = true;
+                        break;
+                    }
+                }
+                if (!check) {
+                    System.out.println(2);
+                }
+            }
+        }
+    }
+}
+
+//7
+//        abba
+//        summuus
+//        xabba
+//        xabbay
+//        comcom
+//        comwwmoc
+//        comwwtmoc
