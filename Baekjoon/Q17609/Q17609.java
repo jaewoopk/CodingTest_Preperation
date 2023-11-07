8eda09ef59a8611b133c74b73d387e48d2b515cf
jaewoopk <jaewoopk000@naver.com>
2023-11-07 16:39:08 +0900

    Day 121 Trying to solve with revsesing string but failed

--- Baekjoon/Q17609/Q17609.java
+++ Baekjoon/Q17609/Q17609.java
@@ -20,13 +20,16 @@ public class Q17609 {
                 System.out.println(0);
             } else {
                 boolean check = false;
-                for (int j = 0; j < str.length(); j++) {
+                for (int j = 0; j < str.length() / 2; j++) {
                     String newStr = str.substring(0, j).concat(str.substring(j + 1, str.length()));
+                    String newStr2 = str.substring(0, str.length() - j - 1).concat(str.substring(str.length() - j, str.length()));
                     //System.out.println(newStr);
                     sb = new StringBuffer(newStr.substring(len / 2, len - 1));
+                    StringBuffer sb2 = new StringBuffer(newStr2.substring(len / 2, len - 1));
                     cmpStr = sb.reverse().toString();
+                    String cmpStr2 = sb2.reverse().toString();
 
-                    if (cmpStr.equals(newStr.substring(0, len / 2))) {
+                    if (cmpStr.equals(newStr.substring(0, len / 2)) || cmpStr2.equals(newStr2.substring(0, len / 2))) {
                         System.out.println(1);
                         check = true;
                         break;
