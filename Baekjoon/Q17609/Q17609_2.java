68f14423a08ec19c4ad15ac0e179cd3d3a5ae562
jaewoopk <jaewoopk000@naver.com>
2023-11-07 17:25:19 +0900

    Day 121 Solve Q17609 with twoPointer

--- Baekjoon/Q17609/Q17609_2.java
+++ Baekjoon/Q17609/Q17609_2.java
@@ -3,30 +3,34 @@ package Baekjoon.Q17609;
 import java.util.*;
 import java.io.*;
 
-public class Q17609 {
+public class Q17609_2 {
+    public static boolean isPalindrome(char[] s, int start, int end) {
+        while (start < end) {
+            if (s[start] != s[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
     public static int twoPointer(char[] str) {
         int start = 0;
         int end = str.length - 1;
-        boolean checker = false;
         while (start < end) {
             if (str[start] == str[end]) {
                 start++;
                 end--;
-            } else if (str[start] != str[end] && !checker) {
-                if (str[start + 1] == str[end]) {
-                    start++;
-                } else if (str[start] == str[end - 1]) {
-                    end--;
+            } else {
+                if (isPalindrome(str, start + 1, end) || isPalindrome(str, start, end - 1)) {
+                    return 1;
                 } else {
                     return 2;
                 }
-                checker = true;
-            } else if (str[start] != str[end] && checker){
-                return 2;
             }
         }
-        if (checker) return 1;
-        return 0;
+        return 0; // 완벽한 회문
     }
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
