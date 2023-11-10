dcd2d7e303a67ce56cd8a21bdaf601144dc5fadf
jaewoopk <jaewoopk000@naver.com>
2023-11-10 12:38:00 +0900

    Day 124 Revise class Node

--- Programmers/PCCP/PCCP2_3/PCCP2_3_answer.java
+++ Programmers/PCCP/PCCP2_3/PCCP2_3_answer.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class PCCP2_3_answer {
     public int solution(int[] menu, int[] order, int k) {
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.k == b.k
+        PriorityQueue<Node2> pq = new PriorityQueue<>((a,b) -> a.k == b.k
                 ? b.pos.compareTo(a.pos) : a.k - b.k);
         int time = 0;
         int people = 0;
@@ -14,14 +14,14 @@ class PCCP2_3_answer {
             if (time < arrivalTime) {
                 time = arrivalTime;
             }
-            pq.add(new Node("arrive", arrivalTime));
+            pq.add(new Node2("arrive", arrivalTime));
 
             time += menu[order[i]];
-            pq.add(new Node("out", time));
+            pq.add(new Node2("out", time));
         }
 
         while(!pq.isEmpty()) {
-            Node tmp = pq.poll();
+            Node2 tmp = pq.poll();
 
             if (tmp.pos.equals("arrive")) {
                 people++;
@@ -34,10 +34,10 @@ class PCCP2_3_answer {
     }
 }
 
-class Node {
+class Node2 {
     String pos;
     int k;
-    Node(String _pos, int _k) {
+    Node2(String _pos, int _k) {
         this.pos = _pos;
         this.k = _k;
     }
