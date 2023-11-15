912015dcc000c0e4cbe641c21f34dd8bb719d244
jaewoopk <jaewoopk000@naver.com>
2023-11-15 16:52:00 +0900

    Day 127 Revise Q1208_2

--- SWEA/D3/Q1208/Q1208_2.java
+++ SWEA/D3/Q1208/Q1208_2.java
@@ -1,32 +1,32 @@
-// wrong way
-//package SWEA.D3.Q1208;
-//
-//import java.util.*;
-//import java.io.*;
-//
-//public class Q1208_2 {
-//    public static void main(String[] args) throws Exception{
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st;
-//
-//        for (int i = 1; i < 11; i++) {
-//            st = new StringTokenizer(br.readLine());
-//            int n = Integer.parseInt(st.nextToken());
-//            PriorityQueue<Integer> minQ = new PriorityQueue<>();
-//            PriorityQueue<Integer> maxQ = new PriorityQueue<>((a,b) -> b - a);
-//
-//            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-//
-//            for (int j = 0; j < 100; i++){
-//                minQ.add(arr[j]);
-//                maxQ.add(arr[j]);
-//            }
-//
-//            for (int j = 0; j < n; j++) {
-//                minQ.add(minQ.poll() + 1);
-//                maxQ.add(maxQ.poll() - 1);
-//            }
-//            System.out.println("#" + i + " " + (maxQ.poll() - minQ.poll()));
-//        }
-//    }
-//}
+ wrong way
+package SWEA.D3.Q1208;
+
+import java.util.*;
+import java.io.*;
+
+public class Q1208_2 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        for (int i = 1; i < 11; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            PriorityQueue<Integer> minQ = new PriorityQueue<>();
+            PriorityQueue<Integer> maxQ = new PriorityQueue<>((a,b) -> b - a);
+
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            for (int j = 0; j < 100; i++){
+                minQ.add(arr[j]);
+                maxQ.add(arr[j]);
+            }
+
+            for (int j = 0; j < n; j++) {
+                minQ.add(minQ.poll() + 1);
+                maxQ.add(maxQ.poll() - 1);
+            }
+            System.out.println("#" + i + " " + (maxQ.poll() - minQ.poll()));
+        }
+    }
+}
