08ec83a022acb1769affe75221e9edb54406adcf
jaewoopk <jaewoopk000@naver.com>
2023-12-07 17:06:03 +0900

    Day 138 Solve Q15686 with backtracking and Implmenetation

--- Baekjoon/Q15686/Q15686.java
+++ Baekjoon/Q15686/Q15686.java
@@ -4,16 +4,45 @@ import java.util.*;
 import java.io.*;
 
 public class Q15686 {
+    static List<Node> chickens;
+    static List<Node> houses;
+    static boolean[] open;
+    static int n, m;
+    static int min = Integer.MAX_VALUE;
+    public static void backTracking(int idx, int cnt) {
+        if (m == cnt) {
+            int sum = 0;
+            for (int i = 0; i < houses.size(); i++) {
+                int tmp = Integer.MAX_VALUE;
+                for (int j = 0; j < chickens.size(); j++) {
+                    if (open[j]) {
+                        tmp = Math.min(tmp, Math.abs(houses.get(i).x - chickens.get(j).x) + Math.abs(houses.get(i).y - chickens.get(j).y));
+                    }
+                }
+                sum += tmp;
+            }
+            min = Math.min(min, sum);
+            return;
+        }
+
+        for (int i = idx; i < chickens.size(); i++) {
+            if (!open[i]) {
+                open[i] = true;
+                backTracking(i + 1, cnt + 1);
+                open[i] = false;
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n, m;
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        Queue<Node> chickens = new LinkedList<>();
-        List<Node> houses = new ArrayList<>();
+        chickens = new ArrayList<>();
+        houses = new ArrayList<>();
 
         for (int i = 1; i < n + 1; i++) {
             st = new StringTokenizer(br.readLine());
@@ -26,25 +55,9 @@ public class Q15686 {
                 }
             }
         }
-
-        int[][] board = new int[chickens.size()][houses.size()];
-
-        int i = 0;
-        while (!chickens.isEmpty()){
-            Node chicken = chickens.poll();
-            for (int j = 0; j < houses.size(); j++) {
-                Node house = houses.get(j);
-                board[i][j] = Math.abs(chicken.x - house.x) + Math.abs(chicken.y - house.y);
-            }
-            i++;
-        }
-        for (i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-
+        open = new boolean[chickens.size()];
+        backTracking(0,0);
+        System.out.println(min);
         br.close();
     }
     static class Node {
